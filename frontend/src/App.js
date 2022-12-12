084dbe5d0a3c663fe36b29d73d029fccd7bd1974
KeenanFernandes <keenan030900@gmail.com>
2022-12-12 21:57:18 +0400

    fixed app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,52 +1,49 @@
 // "Krishna's imports"
-import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import HomeScreen from './HomeScreen';
-import AboutScreen from './AboutScreen';
-import LayoutRoute from './LayoutRoute';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import HomeScreen from "./HomeScreen";
+import AboutScreen from "./AboutScreen";
+import LayoutRoute from "./LayoutRoute";
 // "Krishna's imports end"
-
-
-function App() {
-    return(
-      //"krishna's code"
-        <BrowserRouter>
-            <Switch>
-                <LayoutRoute path="/" exact={true} component={HomeScreen} />
-                <LayoutRoute path="/about" exact={true} component={AboutScreen} />
-            </Switch>
-        </BrowserRouter>
-        //"krishna's code end "
-    );
-}
-/*
-import logo from "./logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-      {/* Jay */}
-
-      {/* Krishna */}
-
-      {/* Keenan */}
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <LayoutRoute path="/" exact={true} component={HomeScreen} />
+        <LayoutRoute path="/about" exact={true} component={AboutScreen} />
+      </Switch>
+    </BrowserRouter>
   );
 }
-*/
+
+// import logo from "./logo.svg";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+
+//       {/* Jay */}
+
+//       {/* Krishna */}
+
+//       {/* Keenan */}
+//     </div>
+//   );
+//}
+
 export default App;
