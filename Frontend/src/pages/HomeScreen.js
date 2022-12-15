4a533f289158467caece275b14b01c4c6f73709a
Krishna Janakinadh <kishorejanakinadh@gmail.com>
2022-12-15 13:28:06 +0400

    Updated folder structure

---
+++ Frontend/src/pages/HomeScreen.js
@@ -0,0 +1,9 @@
+import React from "react";
+// import { Route } from "react-router-dom";
+import Hero from "../components/Hero";
+
+function HomeScreen() {
+  return <Hero />;
+}
+
+export default HomeScreen;
